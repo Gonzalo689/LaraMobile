fac9f4079a57f85ec5b9bfcdb22f2391116cb3c9
Gonzalo Contreras <gonzacd9@gmail.com>
2024-12-01 23:27:10 +0100

    .

--- app/src/main/java/com/example/laramobile/activitys/Home.kt
+++ app/src/main/java/com/example/laramobile/activitys/Home.kt
@@ -1,2 +1,101 @@
 package com.example.laramobile.activitys
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.laramobile.api.RetrofitInstance
+import com.example.laramobile.api.model.Usuario
+
+@Composable
+fun HomeScreen(){
+    var fraseTag by remember { mutableStateOf<List<String>>(emptyList()) }
+    var users by remember { mutableStateOf<List<Usuario>>(emptyList()) }
+    var isLoading by remember { mutableStateOf(true) }
+    var errorMessage by remember { mutableStateOf<String?>(null) }
+
+    LaunchedEffect(Unit) {
+        try {
+            val posts = RetrofitInstance.apiService.getFrases()
+            // Asumiendo que cada post tiene un creador que es un objeto User
+            fraseTag = posts.map { it.tag }
+            users = posts.map { it.creador }
+            isLoading = false
+        } catch (e: Exception) {
+            errorMessage = "Error: ${e.message}"
+            isLoading = false
+        }
+    }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+            ) {
+                when {
+                    isLoading -> LoadingIndicator()
+                    errorMessage != null -> ErrorMessage(errorMessage ?: "Unknown error")
+                    else -> {
+                        // Mostrar la lista de usuarios
+                        LazyColumn(
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            items(fraseTag.zip(users)) { (tag, user) ->
+                                PostItem(tag = tag, user = user)
+                            }
+                        }
+
+
+                    }
+                }
+            }
+        }
+    )
+
+
+}
+
+
+@Composable
+fun LoadingIndicator() {
+    Text(text = "Loading...", modifier = Modifier.padding(16.dp))
+}
+
+@Composable
+fun ErrorMessage(message: String) {
+    Text(text = message, modifier = Modifier.padding(16.dp), color = Color.Red)
+}
+
+
+@Composable
+fun PostItem(tag: String, user: Usuario) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Tag: $tag",
+            style = TextStyle(fontSize = 18.sp)
+        )
+        Text(
+            text = "Creator: ${user.nombre}, Email: ${user.mail}",
+            style = TextStyle(fontSize = 16.sp)
+        )
+    }
+}
--- app/src/main/java/com/example/laramobile/activitys/MainActivity.kt
+++ app/src/main/java/com/example/laramobile/activitys/MainActivity.kt
@@ -1,37 +1,11 @@
 package com.example.laramobile.activitys
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.laramobile.api.RetrofitInstance
-import com.example.laramobile.api.model.Usuario
 import com.example.laramobile.navigation.NavigationWrapper
 import com.example.laramobile.ui.theme.LaraMobileTheme
-import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,82 +20,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun HomeScreen(){
-    var fraseTag by remember { mutableStateOf<List<String>>(emptyList()) }
-    var users by remember { mutableStateOf<List<Usuario>>(emptyList()) }
-    var isLoading by remember { mutableStateOf(true) }
-    var errorMessage by remember { mutableStateOf<String?>(null) }
 
-    LaunchedEffect(Unit) {
-        try {
-            val posts = RetrofitInstance.apiService.getFrases()
-            // Asumiendo que cada post tiene un creador que es un objeto User
-            fraseTag = posts.map { it.tag }
-            users = posts.map { it.creador }
-            isLoading = false
-        } catch (e: Exception) {
-            errorMessage = "Error: ${e.message}"
-            isLoading = false
-        }
-    }
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        content = { innerPadding ->
-            Column(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize()
-            ) {
-                when {
-                    isLoading -> LoadingIndicator()
-                    errorMessage != null -> ErrorMessage(errorMessage ?: "Unknown error")
-                    else -> {
-                        // Mostrar la lista de usuarios
-                        LazyColumn(
-                            modifier = Modifier.weight(1f)
-                        ) {
-                            items(fraseTag.zip(users)) { (tag, user) ->
-                                PostItem(tag = tag, user = user)
-                            }
-                        }
-
-                    }
-                }
-            }
-        }
-    )
-
-
-}
-
-
-@Composable
-fun LoadingIndicator() {
-    Text(text = "Loading...", modifier = Modifier.padding(16.dp))
-}
-
-@Composable
-fun ErrorMessage(message: String) {
-    Text(text = message, modifier = Modifier.padding(16.dp), color = Color.Red)
-}
-
-
-@Composable
-fun PostItem(tag: String, user: Usuario) {
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-    ) {
-        Text(
-            text = "Tag: $tag",
-            style = TextStyle(fontSize = 18.sp)
-        )
-        Text(
-            text = "Creator: ${user.nombre}, Email: ${user.mail}",
-            style = TextStyle(fontSize = 16.sp)
-        )
-    }
-}
 
 
